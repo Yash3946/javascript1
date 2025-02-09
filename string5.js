8648fbaab4e7fd8db4231cd8ffb07aa8bf0e0deb
Yash3946 <patelyash8262@gmail.com>
2025-02-09 09:35:08 +0530

    Add files via upload

---
+++ string5.js
@@ -0,0 +1,20 @@
+// let str = "patel yash"
+// console.log(str.length)  //(length=10)find string length 
+
+// console.log(str[8]) //(position=s)find position of string
+
+            //---->  template literals (it's a special string use of ` `)
+
+// let specialstring =`this is a special string`;
+// console.log(specialstring);
+
+    // EX:
+// let obj = 
+// {
+//     item :"pen" ,
+//     price :10 
+// };
+// console.log("the cost of ",obj.item , "is" ,obj.price,"rupees"); //first style
+
+// let output =`the cost of ${obj.item} is ${obj.price} rupees`; //second style
+// console.log(output)
