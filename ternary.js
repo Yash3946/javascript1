ce5ff8d1caff8da65b7fa834ff57421aead0ec54
yash_3946 <patelyash8262@gmail.com>
2025-02-13 16:16:32 +0530

    .

--- ternary.js
+++ ternary.js
@@ -10,40 +10,26 @@
 // let num2 = parseInt(prompt("Enter second number:"));
 // let num3 = parseInt(prompt("Enter third number:"));
 
-// let count = 0;
 // let sum = 0;
 
-if (num1 >= 35) 
-{
-  count++;
-  sum += num1;
-}
+// if (num1 >= 35) 
+// {
+//   sum += num1;
+// }
 
-if (num2 >= 35) 
-{
-  count++;
-  sum += num2;
-}
+// if (num2 >= 35) 
+// {
+//   sum += num2;
+// }
 
-if (num3 >= 35) 
-{
-  count++;
-  sum += num3;
-}
+// if (num3 >= 35) 
+// {
+//   sum += num3;
+// }
 
-if (count === 3) 
-        {
-        console.log("All numbers are above 35. Sum : ", sum);
-        } 
-else if (count === 2) 
-        {
-        console.log("Two numbers are above 35. Sum : ", sum);
-        } 
-else if (count === 1) 
-        {
-        console.log("One number is above 35. Number : ", sum);
-        } 
-else 
-        {
-        console.log("Please enter numbers above 35.");
-        }
+// console.log(sum)
+
+        //otp generator
+
+        // let otp = Math.floor(100000 + Math.random() * 900000);
+        // console.log("Your OTP is: ", otp);
