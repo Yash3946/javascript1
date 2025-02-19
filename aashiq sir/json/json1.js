a390b8d207b66832135c4c5e49232abb2c31e756
yash_3946 <patelyash8262@gmail.com>
2025-02-19 12:06:17 +0530

    .

---
+++ aashiq sir/json/json1.js
@@ -0,0 +1,66 @@
+        // (1)
+//---------------------------------------------------------
+// var car = {
+//     name : "i20",
+//     engine : "1200cc",
+//     color : ["black","white","blue","yellow"],
+//     details :{
+//         power : "89ps",
+//         tourque : "113nm",
+//     },
+//     isavaible : true,
+// }
+
+// console.log(car.name);
+// console.log(car.color[1]);
+// console.log(car.details.power);
+// for(let i=0;i<car.color.length;i++)
+//     {
+//         console.log(car.color[i]);
+        
+//     }
+
+        // (2)
+//---------------------------------------------------------
+
+// var student = {
+//     id : 12,
+//     name : "jalay",
+//     class : "10th",
+//     age : 17,
+//     ismale : true,
+// }
+// console.log(student.name);
+// console.log(student.class);
+// console.log(student.age);
+
+        // (3)
+//---------------------------------------------------------
+
+// var book = { 
+// var country={
+    
+//     name:"India",
+//     city:"ahmedabad",
+//     state:29,
+//     district:806,
+// }
+// console.log(country.name);
+// console.log(country.city);
+// console.log(country.state);
+// console.log(country.district);
+
+
+        // (4)  
+//---------------------------------------------------------
+
+// var clothing = {
+//     brand: "Zara",
+//     color: "Black",
+//     size: "M",
+//     price: 1500,
+//     material: "Cotton",
+// }
+// console.log(clothing.brand);
+// console.log(clothing.size);
+
